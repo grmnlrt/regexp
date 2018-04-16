40990ae899e34562525e2c73484b37f0be5f895e
Germain Loret <germain.loret@gmail.com>
2018-04-16 18:33:58 +0200

    batch 146 version

--- french_ssn.rb
+++ french_ssn.rb
@@ -1,10 +1,40 @@
+require "date"
+require 'yaml'
+
 # Write a #french_ssn_info method extracting infos from French SSN (Social Security Number) using regexp.
 # example of numbers :
 #   1 84 12 76 451 089 46
 #   2 86 10 75 114 511 75
 #   194073765893575
-#   2890275451089 89
+#   289027545108989
 
 def french_ssn_info(ssn_number)
   # TODO
+  pattern = /^(?<gender>[12])\s?(?<year>\d{2})\s?(?<month>0[1-9]|1[0-2])\s?(?<zip>\d{2}|2[AB])\s?\d{3}\s?\d{3}\s?(?<key>\d{2})/
+  match_data = ssn_number.match(pattern)
+  if match_data
+    # TODO: implement fonction
+    gender = what_gender(match_data["gender"])
+    year = "19#{match_data["year"]}"
+    month = what_month(match_data["month"])
+    zip = what_department(match_data["zip"])
+    # key =
+    return "a #{gender} born in #{month} #{year}, in #{zip}"
+  else
+      return "invalid number"
+  end
+end
+
+def what_gender(number)
+  number == "1" ? "man" : "woman"
+end
+
+def what_month(number)
+  integer = number.to_i
+  Date::MONTHNAMES[integer]
+end
+
+def what_department(number)
+  departments = YAML.load_file('data/french_departments.yml')
+  return departments[number]
 end
--- spec/french_ssn_spec.rb
+++ spec/french_ssn_spec.rb
@@ -1,1 +1,59 @@
 require_relative "../french_ssn"
+
+describe "#french_ssn_info" do
+  it "should return 'number invalid' when passed empty string" do
+    result = french_ssn_info("")
+    expected = "invalid number"
+    expect(result).to eq expected
+  end
+
+  it "should return 'a man born in december 1984, in Seine-Maritime' when passed 1 84 12 76 451 089 46" do
+    result = french_ssn_info("1 84 12 76 451 089 46")
+    expected = "a man born in December 1984, in Seine-Maritime"
+    expect(result).to eq expected
+  end
+end
+
+describe "#what_gender" do
+  it "should return 'woman' when passed 2" do
+    result = what_gender("2")
+    expected = "woman"
+    expect(result).to eq expected
+  end
+
+  it "should return ''man'when passed 1" do
+    result = what_gender("1")
+    expected = "man"
+    expect(result).to eq expected
+  end
+end
+
+
+describe "#what_month" do
+  it "should return 'January' when passed 01" do
+    result = what_month("01")
+    expected = "January"
+    expect(result).to eq expected
+  end
+
+  it "should return 'February' when passed 02" do
+    result = what_month("02")
+    expected = "February"
+    expect(result).to eq expected
+  end
+end
+
+describe "#what_department" do
+  it "should return 'Seine-Maritime' when passed 76" do
+    result = what_department("76")
+    expected = "Seine-Maritime"
+    expect(result).to eq expected
+  end
+
+  it "should return 'Paris' when passed 75" do
+    result = what_department("75")
+    expected = "Paris"
+    expect(result).to eq expected
+  end
+end
+
